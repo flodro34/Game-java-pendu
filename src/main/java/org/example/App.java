5a22af0d22503d957c1abdea997a89b2cf50d0ea
Flo <104248674+flodro34@users.noreply.github.com>
2023-05-29 16:27:53 +0200

    Init

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Binevenue dans notre jeu du Pendu !" );
+
+        //menu : jouer ou quitter
+
+        //getSentence : génère un mot
+
+        //InputWord : lecture clavier sur la lette saisie
+
+        //DisplayWord : affiche une lettre
+    }
+}
